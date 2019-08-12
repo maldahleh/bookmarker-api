c4bb37164a4028a2708a6145628940908abf0efe
Mohammed Al-Dahleh <maldahleh@gmail.com>
2019-08-11 23:30:24 -0400

    Add basic controller methods

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BookmarksService } from './bookmarks.service';
+import { Bookmark } from './bookmarks.model';
 
 @Controller('bookmarks')
 export class BookmarksController {
   constructor(private bookmarksService: BookmarksService) {}
+
+  @Get()
+  getAllBookmarks(): Bookmark[] {
+    return this.bookmarksService.getAllBookmarks();
+  }
+
+  @Post()
+  createTask(@Body() body) {
+    console.log(body);
+  }
 }
--- src/bookmarks/bookmarks.service.ts
+++ src/bookmarks/bookmarks.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { uuid } from 'uuid/v1';
+import { Bookmark } from './bookmarks.model';
 
 @Injectable()
-export class BookmarksService {}
+export class BookmarksService {
+  private bookmarks: Bookmark[] = [];
+
+  getAllBookmarks(): Bookmark[] {
+    return this.bookmarks;
+  }
+
+  createTask(url: string): Bookmark {
+    const bookmark: Bookmark = {
+      id: uuid(),
+      url,
+    };
+
+    this.bookmarks.push(bookmark);
+    return bookmark;
+  }
+}
